4cba427b9d48ac39f7e0a0f0d171996e05f12e48
Lindsay Koprivica <lindsayeplin@gmail.com>
2019-08-01 13:14:21 -0600

    checkpoint-profile: First attempt and implementing profile page for users

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,26 +1,51 @@
 // #1
 const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-// #2
-  createUser(newUser, callback){
-
-// #3
+  // #2
+  createUser(newUser, callback) {
+    // #3
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(newUser.password, salt);
 
-// #4
+    // #4
     return User.create({
       email: newUser.email,
       password: hashedPassword
     })
-    .then((user) => {
-      callback(null, user);
-    })
-    .catch((err) => {
-      callback(err);
-    })
-  }
+      .then(user => {
+        callback(null, user);
+      })
+      .catch(err => {
+        callback(err);
+      });
+  },
+  getUser(id, callback) {
+    let result = {};
+    User.findByPk(id).then(user => {
+      if (!user) {
+        callback(404);
+      } else {
+        result["user"] = user;
 
-}
+        Post.scope({ method: ["lastFiveFor", id] })
+          .findAll()
+          .then(posts => {
+            result["posts"] = posts;
+            Comment.scope({ method: ["lastFiveFor", id] })
+              .findAll()
+              .then(comments => {
+                result["comments"] = comments;
+                callback(null, result);
+              })
+              .catch(err => {
+                callback(err);
+              });
+          });
+      }
+    });
+  }
+};
